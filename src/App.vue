d4eff91f0270c0f731b60512a8eca389cba338d0
pedroivorbgrodrigues <pedroivorbgrodrigues@gmail.com>
2019-03-31 19:59:06 -0300

    Started homepage

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,34 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Book</span>
+        <span class="font-weight-light">Keeper</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
+  name: "App",
+  data() {
     return {
       //
-    }
+    };
   }
-}
+};
 </script>
+
+<style lang="scss">
+.v-content {
+  background-image: url("assets/background.jpg");
+  background-repeat: no-repeat;
+  background-position: bottom;
+  background-size: cover;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-layout row class="text-xs-center" wrap>
+      <v-flex xs4>
+        <v-card class="text-xs-center elevation-4 ma-2" min-height="200">
+          <v-card-title primary-title class="justify-center">
+            <div class="headline text-xs-center">Track your options</div>
+          </v-card-title>
+          <v-card-text class="text-xs-center">
+            Keep track of both the option and its related stock at the same time
+          </v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs4>
+        <v-card class="text-xs-center elevation-4 ma-2" min-height="200">
+          <v-card-title primary-title class="justify-center">
+            <div class="headline text-xs-center">
+              Control your proffit taxes
+            </div>
+          </v-card-title>
+          <v-card-text class="text-xs-center">
+            Calculate and group taxes by due date, save your operation notes and
+            link the compensation ones
+          </v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs4>
+        <v-card class="text-xs-center elevation-4 ma-2" min-height="200">
+          <v-card-title class="justify-center">
+            <div class="headline text-xs-center">
+              See important fact and news
+            </div>
+          </v-card-title>
+          <v-card-text class="text-xs-center">
+            Add feeds, graphs of yelds and price over profit as your own notes
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script></script>
+
+<style></style>
