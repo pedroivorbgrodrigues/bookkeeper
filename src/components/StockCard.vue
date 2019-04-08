1b06cae2d31fd7cac8112875453a2aecbc6f346f
pgalimberti <pedro.galimberti@endurance.com>
2019-04-08 18:45:26 -0300

    Extracted stock card as a component

---
+++ src/components/StockCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card class="clickable">
+    <v-card-title>
+      <v-layout fill-height>
+        <v-flex xs8 style="align-self: center">
+          <div class="display-1">{{stock.title}}</div>
+        </v-flex>
+        <v-flex xs4>
+          <v-img contain max-height="64" :src="stock.image"></v-img>
+        </v-flex>
+      </v-layout>
+    </v-card-title>
+    <v-divider></v-divider>
+    <v-list dense>
+      <v-list-tile>
+        <v-list-tile-content>Vencimento:</v-list-tile-content>
+        <v-list-tile-content class="align-end">{{ stock.dueDate }}</v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>Preço de Entrada:</v-list-tile-content>
+        <v-list-tile-content class="align-end">{{ stock.entryPrice }}</v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "stock-card",
+  props: ["stock"]
+};
+</script>
--- src/views/Stocks.vue
+++ src/views/Stocks.vue
@@ -2,39 +2,14 @@
   <v-container>
     <v-layout row wrap>
       <v-flex xs3 v-for="stock in stocks" :key="stock.title">
-        <v-card
-          :class="{ selected: isSelected(stock) }"
-          class="clickable"
-          @click.native="selectStock(stock)"
-        >
-          <v-card-title>
-              <v-layout fill-height>
-                <v-flex xs8 style="align-self: center">
-                  <div class="display-1">{{stock.title}}</div>
-                </v-flex>
-                <v-flex xs4>
-                  <v-img contain max-height="64" :src="stock.image"></v-img>
-                </v-flex>
-              </v-layout>
-          </v-card-title>
-          <v-divider></v-divider>
-          <v-list dense>
-            <v-list-tile>
-              <v-list-tile-content>Vencimento:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ stock.dueDate }}</v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>Preço de Entrada:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ stock.entryPrice }}</v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-card>
+        <stock-card :key="stock.title" v-bind:stock="stock"/>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import StockCard from '../components/StockCard.vue';
 export default {
   computed: {
     isSelected() {
@@ -56,11 +31,16 @@ export default {
           title: "PETRD305",
           image: require("@/assets/companies/petr.png"),
           dueDate: "15/04/2019",
-          entryPrice: 0.33
+          entryPrice: 0.33,
+          quantity: 400,
+          entryDate: "08/04/2019",
+          sellPrice: 0.55,
+          sellDate: "13/04/2019"
         }
       ]
     };
-  }
+  },
+  components: {StockCard}
 };
 </script>
 
